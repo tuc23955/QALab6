c7e853ffd5330f9029128a0d96c4f163c1aae710
Nicholas G. Selya <tuc23955@temple.edu>
2018-03-13 13:13:20 -0400

    Lab 6 Submission

---
+++ Testerino/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe;
+
+namespace Testerino
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ActiveFieldTest()
+        {
+            
+
+
+        }
+
+
+
+        [TestMethod]
+        public void CurrentPlayerX()
+        {
+            Game g = new Game();
+            Assert.AreEqual('X', g.CurrentPlayer());
+        }
+
+        [TestMethod]
+        public void CurrentPlayerO()
+        {
+            Game g = new Game();
+            g.ChangePlayer();
+            Assert.AreEqual('O', g.CurrentPlayer());
+        }
+
+        [TestMethod]
+        public void XMadeMove()
+        {
+            Game g = new Game();
+            g.MakeMove(0);  //denotes that pplayer X selected square at index 0
+            Assert.AreEqual(g.board[0], 1); //1 in the field denotes that player X marked that square
+        }
+
+        [TestMethod]
+        public void OMadeMove()
+        {
+            Game g = new Game();
+            g.ChangePlayer();
+            g.MakeMove(0);  //denotes that pplayer X selected square at index 0
+            Assert.AreEqual(g.board[0], 0); //0 in the field denotes that player O marked that square
+        }
+
+
+        [TestMethod]
+        public void UnoccupiedSquare()
+        {
+            Game g = new Game();
+            
+            Assert.AreEqual(g.board[0], -1); //-1 in the field denotes that player x marked that square
+        }
+
+
+    }
+}
---
+++ TicTacToe/Game.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Game
+    {
+        public int[] board = new int[9]; //0 used to denote open space, 1 is squared marked X, 2 is square marked O
+       public int turncount = 1;
+
+
+        public Game()
+        {
+            CreateBoard();
+        }
+
+        public void CreateBoard()
+        {
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = -1;
+            }
+        }
+
+        public void ChangePlayer()
+        {
+            turncount++;
+        }
+
+
+        public char CurrentPlayer()
+        {
+            
+
+            if(turncount % 2 == 1)
+            {
+                return 'X';
+            }
+
+            else
+            {
+                return 'O';
+            }
+
+        }
+
+        public void MakeMove(int x)
+        {
+            board[x] = turncount % 2;
+            ChangePlayer();
+
+        }
+
+        public bool SpaceInUse(int x)
+        {
+            return x != -1; //since zero denotes an unused space, this works.
+        }
+
+
+    }
+}
